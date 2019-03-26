76e104db8d3c415b4a97d839f5a97b7e4abd795e
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2019-03-26 16:48:13 +0100

    Update TD3IA.py

--- TD3IA.py
+++ TD3IA.py
@@ -2,3 +2,22 @@
 from __future__ import print_function
 import sys
 from ortools.constraint_solver import pywrapcp
+
+
+    # Nous créons les contraintes 
+    #pas 2 fois le meme nombre sur une meme ligne
+    for l in range(dim):
+        for n in range(dim):
+            for m in range(n+1,dim):
+                solver.Add(grille[l][n] != grille[l][m])
+    #pas 2 fois le meme nombre sur une meme colonne
+    for c in range(dim):
+        for n in range(dim):
+            for m in range(n+1,dim):
+                solver.Add(grille[n][c] != grille[m][c])
+    #pas 2 fois le meme nombre dans une meme case
+    for l0 in range(0,dim,dim_case[0]):
+        for c0 in range(0,dim,dim_case[1]):
+            for n in range(dim):
+                for m in range(n+1,dim):
+                    solver.Add(grille[l0+n//dim_case[1]][c0+n%dim_case[1]] != grille[l0+m//dim_case[1]][c0+m%dim_case[1]])  
