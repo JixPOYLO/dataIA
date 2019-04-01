2e424df2f1ed16fe596c7d97b9200c26116a54ea
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2019-04-01 17:41:33 +0200

    Update sudoooo.py

--- sudoooo.py
+++ sudoooo.py
@@ -1,55 +1,55 @@
 #POYLO-MOUNOT
 
-from __future__ import print_function
+from _future_ import print_function
 
 from ortools.sat.python import cp_model
 
 
 def sudoku():
   
-    # Create the model.
+# creation de notre modele de sudoku
     model = cp_model.CpModel()
-    
     ligne = list(range(0, 9)) # car nombre de lignes = 9 donc les valeurs des i sont 0<=i<9
     region = list(range(0, 3)) # car nombre de cases par regions = 3*3 
 
-
-    grille = {}
+    grille = {}         #declaration de notre grille 
     for i in ligne:     #on parcourt toutes les lignes
         for j in ligne: #on parcourt toutes les colonnes 
-            grille[(i, j)] = model.NewIntVar(1, 9, 'grille %i %i' % (i, j))
+            grille[(i, j)] = model.NewIntVar(1, 9, 'grille %i %i' % (i, j))   #
 
-#definition des contraintes : 
-for i in ligne: 
-model.AddAllDifferent([grille[(i, j)] for j in ligne]) #toutes les valeurs sont differentes sur une ligne
-for j in ligne:
-model.AddAllDifferent([grille[(i, j)] for i in ligne]) #toutes les valeurs sont differentes sur une colonne
+# definition des contraintes : 
 
-for i in region: 
-       for j in region:
-          etuderegion = []
-           for a in region:
-           for b in region:
-               etuderegion.append(grille[(3*i + a, 3*i + b)]) #on utilise append pour ajouter un element a la fin de notre liste
+    for i in ligne:
+        model.AddAllDifferent([grille[(i, j)] for j in ligne]) # valeur differente sur chaque ligne
 
-           model.AddAllDifferent(etuderegion) #toutes les valeurs sont differentes dans une region
+    for j in ligne:
+        model.AddAllDifferent([grille[(i, j)] for i in ligne]) # valeur differente sur chaque colonne
+
+    for i in region: 
+        for j in region:
+            etuderegion = []
+            for a in region:
+                for b in region:
+                    etuderegion.append(grille[(3*i + a, 3*i + b)]) #on utilise append pour ajouter un element a la fin de notre liste
+
+            model.AddAllDifferent(etuderegion) #toutes les valeurs sont differentes dans la region etudiée
 
             
-    # initialisation par rapport a la grille sujet que l'on souhaite resoudre      
-    sujet = [[0, 6, 0, 0, 5, 0, 0, 2, 0], [0, 0, 0, 3, 0, 0, 0, 9, 0],[7, 0, 0, 6, 0, 0, 0, 1, 0], 
-             [0, 0, 6, 0, 3, 0, 4, 0, 0], [0, 0, 4, 0, 7, 0, 1, 0, 0], [0, 0, 5, 0, 9, 0, 8, 0, 0], 
-             [0, 4, 0, 0, 0, 1, 0, 0, 6],[0, 3, 0, 0, 0, 8, 0, 0, 0], [0, 2, 0, 0, 4, 0, 0, 5, 0]]
+    #initialisation par rapport a la grille sujet que l on souhaite resoudre 
+     
+    sujet = [[5, 3, 0, 0, 7, 0, 0, 0, 0], [6, 0, 0, 1, 9, 5, 0, 0, 0],[0, 9, 8, 0, 0, 0, 0, 6, 0], 
+             [8, 0, 0, 0, 6, 0, 0, 0, 3], [4, 0, 0, 8, 0, 3, 0, 0, 1], [7, 0, 0, 0, 2, 0, 0, 0, 6], 
+             [0, 6, 0, 0, 0, 0, 2, 8, 0],[0, 0, 0, 4, 1, 9, 0, 0, 5], [0, 0, 0, 0, 8, 0, 0, 7, 9]]
     for i in ligne:
      for j in ligne:
           if sujet[i][j]:
                 model.Add(grille[(i, j)] == sujet[i][j])
 
-    # Resolution et sortie de la solution dans la console
+    # resolution et sortie de la solution dans la console
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
     if status == cp_model.FEASIBLE:    #FEASIBLE = resolution possible mais resultat pas forcement optimal
         for i in ligne:
             print([int(solver.Value(grille[(i, j)])) for j in ligne])
-
 
 sudoku()
